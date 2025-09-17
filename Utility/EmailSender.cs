65d50735c4dbeca7d99974365fb49dece757b16d
Khalid <abrahem34481@gmail.com>
2025-09-17 16:27:21 +0300

    add email sender

--- Utility/EmailSender.cs
+++ Utility/EmailSender.cs
@@ -1,18 +1,45 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+
 
 namespace Utility
 {
     public class EmailSender : IEmailSender
-    {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            return Task.CompletedTask;
+    {        
+
+        public EmailSender()
+        {            
         }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {            
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Lazmek Store", "Lazmekstore@gmail.com"));
+                message.To.Add(new MailboxAddress("", email));
+                message.Subject = subject;
+
+                message.Body = new TextPart("html")
+                {
+                    Text = htmlMessage
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync("smtp.gmail.com", 465, true);
+                    await client.AuthenticateAsync("Lazmekstore@gmail.com", "******");
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"error throw send email :{ex.Message}");
+            }
+        }
+
     }
 }
